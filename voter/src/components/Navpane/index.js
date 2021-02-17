0fe2bb96788bae6e6d0fa850fc7edebbe31e963b
SurlyMae <surlymae@gmail.com>
2021-02-17 15:57:54 -0700

    WIP: sorting data

--- voter/src/components/Navpane/index.js
+++ voter/src/components/Navpane/index.js
@@ -2,22 +2,32 @@ import React from "react";
 import StateLink from "../StateLink";
 
 function Navpane({ members }) {
-  const getCircularReplacer = () => {
-    const seen = new WeakSet();
-    return (key, value) => {
-      if (typeof value === "object" && value !== null) {
-        if (seen.has(value)) {
-          return;
-        }
-        seen.add(value);
-      }
-      return value;
-    };
-  };
-  console.log(`props are ${JSON.stringify(members, getCircularReplacer())}`);
+  let stateArray = [];
+  members.map((member) => {
+    stateArray.push({ id: member.id, state: member.state });
+  });
+
+//   let states = [];
+//   states.push(a.state);
+//   if (!states.includes(b.state)) {
+//     states.push(b.state);
+//   }
+//   console.log(`${states}`);
+
+//   if (a.state < b.state) {
+//     return -1;
+//   }
+//   if (a.state > b.state) {
+//     return 1;
+//   }
+//   return 0;
+// });
+  let sortedArray = stateArray.sort((a, b) => {
+   
+  console.log(`${JSON.stringify(sortedArray)}`);
   return (
     <nav className="nav flex-column">
-      {members.map((member) => {
+      {states.map((member) => {
         return <StateLink key={member.id} state={member.state}></StateLink>;
       })}
     </nav>
