3d2c5af2784ed3ef81a1a3656a051368ab64e51f
Dustin LaMarr <dustin.lamarr@gmail.com>
2021-02-17 08:31:07 -0700

    init commit

---
+++ voter/src/components/Container/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Container() {
+    return(
+    <div className="container">
+        <img src="/images/voter-sq.png" alt="voter"></img>
+    </div>
+    )
+}
+
+export default Container;
